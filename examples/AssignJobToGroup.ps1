030d6c4aa666406390e9cc251ecb06898fa2e294
Tobias Merkens <Tobias.Merkens@caritasmuenchen.de>
2020-01-24 12:51:27 +0100

    save work :))

---
+++ examples/AssignJobToGroup.ps1
@@ -0,0 +1,19 @@
+# Import Module
+Import-Module -Name bConnect
+# Get Crediential
+$apiCred = Get-Credential -Message "bConnect" -UserName "user@domain.local"
+# Initialize bConnect Module
+Initialize-bConnect -Server "SRV-BARAMUNDI" -Credentials $apiCred
+# Search for an static Group
+$staticGroup = Search-bConnectGroup -Term "NO: Office 2019"
+# Get Endpoints
+$endpoints = Get-bConnectEndpoint -StaticGroupGuid $staticGroup.Id
+
+foreach($endpoint in $endpoints){
+    $endpoint.Id
+}
+
+# Search for a job
+$job = Search-bConnectJob -Term "Inst: Office 2019"
+# Assign the job to the endpoint by creating a new job instance
+New-bConnectJobInstance -EndpointGuid $endpoint.Id -JobGuid $job.Id -Initiator "PS-bConnect" -StartIfExists
