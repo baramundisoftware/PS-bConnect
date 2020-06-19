dafd2fae152fbdf44f30ff94b2543fd0be184365
baramundi-skrusbersky <60090096+baramundi-skrusbersky@users.noreply.github.com>
2020-06-19 02:24:46 -0400

    Add Controller ServerState (#21)
    * Add Controller ServerState

---
+++ bConnect/Public/Get-bConnectServerState.ps1
@@ -0,0 +1,15 @@
+Function Get-bConnectServerState() {
+    <#
+        .Synopsis
+            Get baramundi server and sub services state.        
+        .Outputs
+            ServerState object representing the current state of the baramundi server and its sub services 
+    #>
+
+    $_connectVersion = Get-bConnectVersion
+    If($_connectVersion -ge "1.0") {
+        return Invoke-bConnectGet -Controller "ServerState" -Version $_connectVersion
+    } else {
+        return $false
+    }
+}
