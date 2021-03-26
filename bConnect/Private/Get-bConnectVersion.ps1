73ec23f7fb30c730068c600050d11f27f9b1c9bb
Alexander Haugk <alexander.haugk@baramundi.com>
2021-03-26 09:25:45 +0100

    Added support for 2021 R1, removed 2018 and lower

--- bConnect/Private/Get-bConnectVersion.ps1
+++ bConnect/Private/Get-bConnectVersion.ps1
@@ -15,40 +15,20 @@ Function Get-bConnectVersion() {
         $_bmsVersion = $_info.bMSVersion
 
         switch -Wildcard ($_bmsVersion) {
-            "14.0.*" {
-                Write-Warning "DEPRECATED! bConnect 2014R1"
-            }
-
-            "14.2.*" {
-                Write-Warning "DEPRECATED! bConnect 2014R2"
-            }
-
-            "15.*" {
-                Write-Verbose "DEPRECATED! bConnect 2015R1 or newer"
-            }
-
-			"16.*" {
-				Write-Verbose "DEPRECATED! bConnect 2016R1 or newer"
-			}
-
-			"17.*" {
-				Write-Verbose "DEPRECATED! bConnect 2017R1 or newer"
-			}
-
-			"18.*" {
-				Write-Verbose "bConnect 2018R1 or newer"
-			}
-
 			"19.*" {
-				Write-Verbose "bConnect 2019R1 or newer"
+				Write-Verbose "DEPRECATED! bConnect 2019 R1 or newer"
 			}
 
 			"20.*" {
-				Write-Verbose "bConnect 2020R1 or newer"
+				Write-Verbose "bConnect 2020 R1 or newer"
+			}
+
+            "21.*" {
+				Write-Verbose "bConnect 2021 R1 or newer"
 			}
 
             default {
-                Write-Warning "NOT SUPPORTED! Unknown bConnect Version -> Fallback to $($script:_bConnectFallbackVersion)"
+                Write-Warning "UNSUPPORTED bMS Version $($_bmsVersion)! Unknown bConnect Version -> Fallback to $($script:_bConnectFallbackVersion)"
                 $_bcVersion = $script:_bConnectFallbackVersion
             }
         }
