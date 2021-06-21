a11cc98d7ca9c84b4b87044edc707c3897bda90b
Tobias Merkens <Tobias.Merkens@caritasmuenchen.de>
2021-06-21 08:31:55 +0200

    add Get-bConnectIpNetwork

---
+++ bConnect/Public/Get-bConnectIpNetwork.ps1
@@ -0,0 +1,36 @@
+Function Get-bConnectIpNetwork() {
+    <#
+        .Synopsis
+            Get Ip Network based on Name or Id.
+        .Parameter Name
+            Valid Ip Network Name.
+        .Parameter Id
+            Valid GUID of an Ip Network.
+        .Outputs
+            Array of IpNetworks (see bConnect documentation for more details).
+    #>
+
+    Param (
+        [string]$Name,
+        [string]$Id
+        )
+
+    $_connectVersion = Get-bConnectVersion
+    If($_connectVersion -ge "1.0") {
+        If($Name) {
+            $_body = @{
+                Name = $Name
+            }
+        }
+
+        If($Id) {
+            $_body = @{
+                Id = $Id
+            }
+        }
+
+        return Invoke-bConnectGet -Controller "IpNetworks" -Version $_connectVersion -Data $_body
+    } else {
+        return $false
+    }
+}
