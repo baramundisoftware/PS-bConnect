89fe4144643da22c53ed159a17d089bb244c629d
Tobias Merkens <Tobias.Merkens@caritasmuenchen.de>
2021-06-24 11:25:41 +0200

    add "Remove-bConnectIpNetwork"

---
+++ bConnect/Public/Remove-bConnectIpNetwork.ps1
@@ -0,0 +1,42 @@
+﻿Function Remove-bConnectIpNetwork() {
+    <#
+        .Synopsis
+            Remove specified IpNetwork.
+        .Parameter IpNetworkGuid
+            Valid GUID of a IpNetwork.
+        .Parameter IpNetwork
+            Valid IpNetwork Object.
+        .Outputs
+            Bool
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    [OutputType("System.Boolean")]
+    Param (
+        [string]$IpNetworkGuid,
+        [PSCustomObject]$IpNetwork
+    )
+
+    $_connectVersion = Get-bConnectVersion
+    If($_connectVersion -ge "1.0") {
+        If(![string]::IsNullOrEmpty($IpNetworkGuid)) {
+            $_body = @{
+                Id = $IpNetworkGuid
+            }
+        } elseif (![string]::IsNullOrEmpty($IpNetwork.Id)) {
+            $_body = @{
+                Id = $IpNetwork.Id
+            }
+        } else {
+            return $false
+        }
+
+        if($PSCmdlet.ShouldProcess($_body.Id, "Remove IpNetwork and all associated data from the database.")){
+            return Invoke-bConnectDelete -Controller "IpNetworks" -Version $_connectVersion -Data $_body
+        } else {
+            return $false
+        }
+    } else {
+        return $false
+    }
+}
