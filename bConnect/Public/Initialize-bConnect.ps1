cc9de23e184242beeb10588cbb97427b6fafdd10
Tobias <admin+github@t0biii.de>
2022-01-26 21:05:11 +0100

    moved Scriptblock to Initialize-bConnect

--- bConnect/Public/Initialize-bConnect.ps1
+++ bConnect/Public/Initialize-bConnect.ps1
@@ -19,6 +19,26 @@ Function Initialize-bConnect() {
         [switch]$AcceptSelfSignedCertifcate
     )
 
+    # fallback bConnect version
+    $script:_bConnectFallbackVersion = "v1.0"
+
+    # overwrite Invoke-RestMethod timeout
+    $script:_ConnectionTimeout = 0
+
+    # Only to ignore certificates errors (self-signed)
+    Add-Type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+public class ignoreCertificatePolicy : ICertificatePolicy {
+    public ignoreCertificatePolicy() {}
+    public bool CheckValidationResult(ServicePoint sPoint, X509Certificate cert, WebRequest wRequest, int certProb) { return true; }
+}
+"@
+
+    # init the connection (uri and credentials)
+    $script:_connectInitialized = $false
+
     If($AcceptSelfSignedCertifcate) {
         [System.Net.ServicePointManager]::CertificatePolicy = New-Object ignoreCertificatePolicy
     }
