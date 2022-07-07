cd7dc4f64a9fe8a430ff7a7147f03864113d4095
Tobias <admin+github@t0biii.de>
2022-07-07 08:34:16 +0200

    add bms 2022 to supportet Versions

--- bConnect/Private/Get-bConnectVersion.ps1
+++ bConnect/Private/Get-bConnectVersion.ps1
@@ -16,18 +16,22 @@ Function Get-bConnectVersion() {
         $_bmsVersion = $_info.bMSVersion
 
         switch -Wildcard ($_bmsVersion) {
-			"19.*" {
-				Write-Verbose "DEPRECATED! bConnect 2019 R1 or newer"
-			}
+            "19.*" {
+                Write-Verbose "DEPRECATED! bConnect 2019 R1 or newer"
+            }
 
-			"20.*" {
-				Write-Verbose "bConnect 2020 R1 or newer"
-			}
+            "20.*" {
+                Write-Verbose "bConnect 2020 R1 or newer"
+            }
 
             "21.*" {
-				Write-Verbose "bConnect 2021 R1 or newer"
-			}
-
+                Write-Verbose "bConnect 2021 R1 or newer"
+            }
+			
+            "22.*" {
+                Write-Verbose "bConnect 2022 R1 or newer"
+            }
+	    
             default {
                 Write-Warning "UNSUPPORTED bMS Version $($_bmsVersion)! Unknown bConnect Version -> Fallback to $($script:_bConnectFallbackVersion)"
                 $_bcVersion = $script:_bConnectFallbackVersion
